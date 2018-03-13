98b1d28f8c376eaf712fc41698ffdaaeebc819d8
vshishkov <vshishkov@amberdata.ru>
2018-03-13 19:48:11 +0300

    initial

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject gepher "0.1.0-SNAPSHOT"
+  :description "convert edges to gexf"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [hiccup "1.0.5"]
+                 [proto-repl "0.3.1"]
+                 [clj-time "0.14.2"]])
