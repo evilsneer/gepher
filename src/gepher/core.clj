4b42bc134a11cd7f0eab6cae99e678b60821eb50
vshishkov <vshishkov@amberdata.ru>
2018-03-14 20:19:43 +0300

    fix

--- src/gepher/core.clj
+++ src/gepher/core.clj
@@ -17,24 +17,25 @@
 
 (defn edges->gexf
   "Convert `edges`= [({:id 1 :label 42} {:id 2 :label 34})] to gexf file"
-  [edges]
-  (let [graph-body [:graph {:mode "static" :defaultedgetype "directed"}
-                           [:attributes {:class "node"}
-                                        [:attribute {:id 0 :title "deep" :type "float"}]]
-                           [:nodes (->> edges
-                                        flatten
-                                    (map #(vector :node {:id (:id %) :label (:label %)} [:attvalue {:for 0 :value (:deep %)}])))]
-                           [:edges (->> edges
-                                     (map #(map node->id %))
-                                     (map-indexed vector)
-                                     (map #(let [index (first %)
-                                                 edge (second %)]
-                                                (vector :edge {:id index :source (first edge) :target (second edge)}))))]]]
-       (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-            (hiccup/html  [:gexf {:xmlns "http://www.gexf.net/1.2draft" :version "1.2"}
-                           [:meta {:lastmodifieddate (now)}
-                             [:creator "Gepher"]
-                             [:description "DESCRIPTION"]
-                             graph-body]]))))
+  ([edges] (edges->gexf edges "directed"))
+  ([edges type]
+   (let [graph-body [:graph {:mode "static" :defaultedgetype type}
+                            [:attributes {:class "node"}
+                                         [:attribute {:id 0 :title "deep" :type "float"}]]
+                            [:nodes (->> edges
+                                         flatten
+                                     (map #(vector :node {:id (:id %) :label (:label %)} [:attvalue {:for 0 :value (:deep %)}])))]
+                            [:edges (->> edges
+                                      (map #(map node->id %))
+                                      (map-indexed vector)
+                                      (map #(let [index (first %)
+                                                  edge (second %)]
+                                                 (vector :edge {:id index :source (first edge) :target (second edge)}))))]]]
+        (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+             (hiccup/html  [:gexf {:xmlns "http://www.gexf.net/1.2draft" :version "1.2"}
+                            [:meta {:lastmodifieddate (now)}
+                              [:creator "Gepher"]
+                              [:description "DESCRIPTION"]
+                              graph-body]])))))
 
 ;(edges->gexf test-data)
