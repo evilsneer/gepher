cb8c481c32cee2fedf62651c63038bc1c4043326
vshishkov <vshishkov@amberdata.ru>
2019-02-20 15:30:24 +0300

    clean

--- src/gepher/core.clj
+++ src/gepher/core.clj
@@ -16,11 +16,6 @@
 (def ID :id)
 (def MAIN-ARGS [LABEL ID])
 
-(def td ['({:label ":ROOT", :id 0, :deep 1 :food "3"}
-           {:label "project.clj", :id 13, :deep 2})
-         '({:label ":ROOT", :id 0, :deep 1}
-           {:label "LICENSE", :id 14, :deep 2})])
-
 ;;; Time format functions for gexf metadata
 (defn- format-time [t]
   "Format time for this project. `t` is localtime obj."
@@ -46,13 +41,16 @@
 (defn- update-map [f m]
   "Apply `f` function to `m` hash-map "
   ; (reduce-kv (fn [m k v]
-    ; (assoc m k (f v))) {} m)) ;; hmm lets try something simplier
+  ; (assoc m k (f v))) {} m)) ;; hmm lets try something simpler
   (zipmap (keys m) (map f (vals m))))
 
 
 ;;; Conversions
 (defn- node->id [node]
   (:id node))
+
+(defn- node->to-edge-label [node]
+  (:to-label node))
 
 
 ;; Attributes
@@ -68,7 +66,7 @@
   (->> e
     flatten
     (mapcat identity)
-    (filter (fn [[k v]] (not (in? k [:id :label]))))
+    (filter (fn [[k _]] (not (in? k [:id :label :to-label]))))
     (map (fn [[k v]] {k [v]}))
     (apply merge-with into)
     (update-map coll->gexf-attr-type)))
@@ -97,37 +95,37 @@
 (defn- node->attvalues [n att-id-map]
   "Convert node to attributes representation in hiccup/html gexf format."
   (->> att-id-map
-    (filter (fn [[k v]] (some? (k n))))
+    (filter (fn [[k _]] (some? (k n))))
     (map (fn [[k v]] [:attvalue {:for v :value (k n)}]))
     (apply vector)))
 
 
-;; Nodes
+;;; Nodes representation
 (defn- node->node-representation [att-id-map n]
   "Convert node to representation in hiccup/html gexf format."
-  (into [] 
-    (concat 
-      [:node {:id (:id n) :label (:label n)}] 
+  (into []
+    (concat
+      [:node {:id (:id n) :label (:label n)}]
       (node->attvalues n att-id-map))))
 
 (defn edges->node-representations [edges att-id-map]
   "Convert edges to node representations in hiccup/html gexf format."
   (->> edges
-       flatten
-       set
-       (map (partial node->node-representation att-id-map))))
+    flatten
+    set
+    (map (partial node->node-representation att-id-map))))
 
 
 ;; Edges
 (defn- edge->edge-representation [e]
   "Convert one edge to representation in hiccup/html gexf format."
-  (let [[i [fe se]] e]
-    [:edge {:id i :source fe :target se}]))
+  (let [[i [label fe se]] e]
+    [:edge {:id i :source fe :target se :label label}]))
 
 (defn edges->edge-representations [edges]
   "Convert edges to edges representation in hiccup/html gexf format."
   (->> edges
-    (map #(map node->id %))
+    (map (fn [nodes] (cons (node->to-edge-label (last nodes)) (map node->id nodes))))
     (map-indexed vector)
     (map edge->edge-representation)))
 
@@ -135,25 +133,23 @@
   "Convert `edges`= [({:id 1 :label 42} {:id 2 :label 34})] to gexf file"
   ([edges] (edges->gexf edges "directed"))
   ([edges type]
-   (let [attr->attrid (edges->attribute-attrid-map edges)]
-    (let [graph-body [:graph {:mode "static" :defaultedgetype type}
-                             (edges->nodes-attributes-representation edges)
-                             [:nodes (edges->node-representations edges attr->attrid)]
-                             [:edges (edges->edge-representations edges)]]]
-         (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-              (hiccup/html  [:gexf {:xmlns "http://www.gexf.net/1.2draft" :version "1.2"}
-                             [:meta {:lastmodifieddate (now)}
-                               [:creator "Gepher"]
-                               [:description "DESCRIPTION"]]
-                             graph-body]))))))
+   (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+     (hiccup/html [:gexf {:xmlns "http://www.gexf.net/1.2draft" :version "1.2"}
+                   [:meta {:lastmodifieddate (now)}
+                    [:creator "Gepher"]
+                    [:description "DESCRIPTION"]]
+                   [:graph {:mode "static" :defaultedgetype type}
+                    (edges->nodes-attributes-representation edges)
+                    [:nodes (edges->node-representations edges (edges->attribute-attrid-map edges))]
+                    [:edges (edges->edge-representations edges)]]]))))
 
 (defn -main [& args]
   "Start point."
   (let [opt (:options (parse-opts args cli-options))]
     (if-let [file (get opt :file)]
       (let [content (->> file
-                         slurp
-                         json/read-str
-                         (map #(map str-keys->keywords-keys %)))]
+                      slurp
+                      json/read-str
+                      (map #(map str-keys->keywords-keys %)))]
         (println (edges->gexf content)))
       (println "Specify file with -f arg"))))
